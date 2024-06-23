87bf05c123b6b79f128a18678b841851917d7508
M1nGW-w64 <2439932768@qq.com>
2024-06-23 15:02:18 +0800

    support searching by regx; support searching in multiple files

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,69 @@
 use std::collections::HashMap;
 use std::{error::Error, fs};
+use std::path::PathBuf;
 use clap::Parser;
+use regex::Regex;
+use itertools::Itertools;
 
 pub struct Config {
   pub query: String,
-  pub files: String,
+  pub files: Vec<String>,
   pub ignore_case: bool,
+  pub reg: bool,
 }
 
 #[derive(Parser)]
-#[command(version, author, about)]
+#[command(version = "0.1.0", author = "984705", about = "a minigrep for study")]
 pub struct Cli {
   query: String,
-  files: String,
+  files: Vec<String>,
 
+  /// ignore case or not
   #[arg(short, long)]
   ignore_case: bool,
+
+  /// partern is a regex
+  #[arg(short, long)]
+  regex: bool,
 }
 
 impl Config {
   pub fn build() -> Result<Config, &'static str> {
     let cli = Cli::parse();
+    
     let query = cli.query.clone();
     let files = cli.files.clone();
     let ignore_case = cli.ignore_case.clone();
-    Ok(Config{query, files, ignore_case})
+    let reg = cli.regex.clone();
+    
+    Ok(Config{query, 
+              files, 
+              ignore_case, 
+              reg})
   }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-  let contents = fs::read_to_string(&config.files)?;
-  let f = config.files;
+  for file in config.files.iter() {
+    let contents = fs::read_to_string(file)?;
+    
+    let results = if config.reg {
+      search_regex(&config.query, &contents)
+    } else if config.ignore_case {
+      search_insentitve(&config.query, &contents)
+    } else {
+      search(&config.query, &contents)
+    };
 
-  // using hashmap instead of vector to record the query word in which line
-  let results = if config.ignore_case {
-    search_insentitve(&config.query, &contents)
-  } else {
-    search(&config.query, &contents)
-  };
+    println!("{:?}", fs::canonicalize(PathBuf::from(file)).unwrap());
 
-  for (line_no, line) in results.iter() {
-    println!("In file {f}, line {line_no} : {line}");
+    for (line_no, line) in results.iter().sorted_by_key(|x| x.0) {
+      println!("Line {line_no} : {line}");
+    }
+
+    print!("\n");
   }
-  
+
   Ok(())
 }
 
@@ -75,6 +96,20 @@ pub fn search_insentitve<'a>(query: &str, contents: &'a str) -> HashMap<i32, &'a
   results
 }
 
+pub fn search_regex<'a>(query: &str, contents: &'a str) -> HashMap<i32, &'a str> {
+  let mut results = HashMap::new();
+  let mut line_no = 0;
+  let re = Regex::new(query).unwrap();
+
+  for line in contents.lines() {
+    line_no += 1;
+    if re.captures(line).is_some() {
+      results.insert(line_no, line);
+    }
+  }
+  
+  results
+}
 // hashmap init marco, like vec!
 macro_rules! hashmap {
   ($( $key: expr => $val: expr ),*) => {{
@@ -87,6 +122,14 @@ macro_rules! hashmap {
 #[cfg(test)]
 mod tests {
   use super::*;
+
+  #[test]
+  fn case_without_parameter() {
+    let query = "";
+    let contents = "";
+
+    assert_eq!(hashmap!(), search(query, contents));
+  }
 
   #[test]
   fn case_basic() {
@@ -119,6 +162,17 @@ safe, fast, productive.
 Trust me.";
 
     assert_eq!( hashmap![1 => "Rust:", 3 => "Trust me."], search_insentitve(query, contents));
+  }
+
+  #[test]
+  fn case_regex() {
+    let query = r"[A-Z][a-z]{1,3}";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Trust me.";
+
+    assert_eq!( hashmap![1 => "Rust:", 3 => "Trust me."], search_regex(query, contents));
   }
 
 }
